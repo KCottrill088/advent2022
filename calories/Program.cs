e4de9997d533c675602da9733fbef28dc1d95016
kevin <KCottrill088@GMail.com>
2022-12-01 05:17:16 -0600

    Oops.

--- calories/Program.cs
+++ calories/Program.cs
@@ -16,5 +16,6 @@ foreach (var line in File.ReadLines(@"/home/kevin/repos/advent2022/data/calories
         sum += number;
     }
 }
+calories.Add(sum);
 
 Console.WriteLine(calories.OrderByDescending(x => x).ToList().GetRange(0, 3).Sum(x => (long)x));
