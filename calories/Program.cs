3e92d06d38e2ca886f6a6fd8e8d2716d732c5879
kevin <KCottrill088@GMail.com>
2022-12-01 05:09:04 -0600

    Top three - second star

--- calories/Program.cs
+++ calories/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System.Numerics;
 
 BigInteger sum= 0;
-BigInteger max = 0;
 
-foreach (var line in File.ReadLines(@"/home/kevin/repos/advent2022/data/calories.example"))
+List<BigInteger> calories = new List<BigInteger>();
+
+foreach (var line in File.ReadLines(@"/home/kevin/repos/advent2022/data/calories.dat"))
 {
     if (string.IsNullOrWhiteSpace(line))
     {
+        calories.Add(sum);
         sum = 0;
     }
     else if (BigInteger.TryParse(line, out var number))
     {
         sum += number;
     }
-    max = BigInteger.Max(sum, max);
 }
-Console.WriteLine($"Max {max}");
+
+Console.WriteLine(calories.OrderByDescending(x => x).ToList().GetRange(0, 3).Sum(x => (long)x));
