b1a97f6c21fa3afe16e1bf9f66098359157f1e2f
kevin <KCottrill088@GMail.com>
2022-12-05 05:03:13 -0600

    Add day5 skeleton

---
+++ day5/Day5.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Day5.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ day5/Day5/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Day5;
+public class Class1
+{
+
+}
