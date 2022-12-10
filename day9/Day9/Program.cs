881e20a38faa35c1c7ad4b9cee30bfa6d37139bf
kevin <KCottrill088@GMail.com>
2022-12-10 08:01:10 -0600

    Add skeleton projects through day10

--- day9/Day9/Program.cs
+++ day9/Day9/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var moves = File.ReadAllLines("data/moves.example");
+
+var position = new Pair(0, 0);
+foreach (var move in moves)
+{
+    var direction = move[0];
+    var distance = int.TryParse(move.Substring(2), out var d) ? d : 0;
+    Console.WriteLine($"{direction}\t{distance}");
+}
+
+record Pair(int X, int Y);
