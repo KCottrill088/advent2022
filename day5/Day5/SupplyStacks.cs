27db6a31f7764311d734d03cc1b254d0006666b6
kevin <KCottrill088@GMail.com>
2022-12-11 10:07:17 -0600

    Day5, part 1 solution

--- day5/Day5/SupplyStacks.cs
+++ day5/Day5/SupplyStacks.cs
@@ -1,26 +1,64 @@
 ﻿using System.Text.RegularExpressions;
 
-var crates = File.ReadAllLines("data/crates.example");
-
-var stacks = new[] { new Stack<char>() };
-
-foreach (var crate in InputHandler.GetCrates(crates))
+var input = File.ReadAllLines("data/crates.example");
+var crates = new List<string>();
+foreach (var row in input)
 {
-    foreach (var supply in crate)
+    if (Regex.Match(row, @"^(\s+\d+)+").Success) break;
+    crates.Add(row);
+}
+crates.Reverse();
+
+var stacks = new List<Stack<char>>();
+foreach (var row in crates)
+{
+    for (var i = 0; i < row.Length; i += 4)
     {
-        
+        if (i / 4 == stacks.Count)
+            stacks.Add(new Stack<char>());
+
+        var item = row[i + 1];
+        if (item != ' ')
+            stacks[i / 4].Push(item);
     }
 }
 
-var i = 0;
-while (!Regex.Match(crates[i], @"^\s+\d").Success)
+var moves = new List<Move>();
+int quantity, source, destination;
+foreach (var row in input)
 {
-    var part = crates[i].Take(4);
-    Console.WriteLine(part);
-    Console.WriteLine(crates[i]);
-    i++;
+    if (!Regex.Match(row, @"^move").Success) continue;
+    if (row.Length == 18)
+    {
+        quantity = int.Parse(row.Substring(5, 1));
+        source = int.Parse(row.Substring(12, 1)) - 1;
+        destination = int.Parse(row.Substring(17, 1)) - 1;
+    }
+    else
+    {
+        quantity = int.Parse(row.Substring(5, 2));
+        source = int.Parse(row.Substring(13, 1)) - 1;
+        destination = int.Parse(row.Substring(18, 1)) - 1;
+    }
+    moves.Add(new Move(quantity, source, destination));
 }
 
+foreach (var move in moves)
+{
+    for (var i = 0; i < move.Quantity; i++)
+    {
+        var item = stacks[move.Source].Pop();
+        stacks[move.Destination].Push(item);
+    }
+}
+
+foreach (var stack in stacks)
+{
+    Console.Write(stack.Peek());
+}
+Console.WriteLine();
+
+public record Move(int Quantity, int Source, int Destination);
 public static class InputHandler
 {
     public static IEnumerable<string> SplitRow(string input)
