6f533762f92c568f671ae5fc4df9334b315a3773
kevin <KCottrill088@GMail.com>
2022-12-15 04:13:18 -0600

    Filling in skeletons for missing days

---
+++ day13/Day13.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Day13.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ day14/Day14.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Day14.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
