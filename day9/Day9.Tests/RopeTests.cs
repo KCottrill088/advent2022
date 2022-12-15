66eb03e6eeaeff9f8c00383f5f94561bbc043d7e
kevin <KCottrill088@GMail.com>
2022-12-14 20:44:43 -0600

    WIP LongRope

--- day9/Day9.Tests/RopeTests.cs
+++ day9/Day9.Tests/RopeTests.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace Day9.Tests;
@@ -5,17 +6,17 @@ namespace Day9.Tests;
 public class RopeTests
 {
     [Theory]
-    [InlineData('U', 1, 0, 1, 0, 0)]
-    [InlineData('U', 2, 0, 2, 0, 1)]
-    [InlineData('D', 1, 0, -1, 0, 0)]
-    [InlineData('D', 2, 0, -2, 0, -1)]
-    [InlineData('R', 1, 1, 0, 0, 0)]
-    [InlineData('R', 2, 2, 0, 1, 0)]
-    [InlineData('L', 1, -1, 0, 0, 0)]
-    [InlineData('L', 2, -2, 0, -1, 0)]
-    public void MoveTests(char direction, int distance, int headX, int headY, int tailX, int tailY)
+    [InlineData('U', 1, 0, 1, 0, 0, 1)]
+    [InlineData('U', 2, 0, 2, 0, 1, 2)]
+    [InlineData('D', 1, 0, -1, 0, 0, 1)]
+    [InlineData('D', 2, 0, -2, 0, -1, 2)]
+    [InlineData('R', 1, 1, 0, 0, 0, 1)]
+    [InlineData('R', 2, 2, 0, 1, 0, 2)]
+    [InlineData('L', 1, -1, 0, 0, 0, 1)]
+    [InlineData('L', 2, -2, 0, -1, 0, 2)]
+    public void MoveTests(char direction, int distance, int headX, int headY, int tailX, int tailY, int expectedVisited)
     {
-        var rope = new Rope();
+        var rope = new Rope(2);
         var move = new MoveInput(direction, distance);
         var expectedHeadPosition = new Position(headX, headY);
         var expectedTailPosition = new Position(tailX, tailY);
@@ -24,5 +25,17 @@ public class RopeTests
         
         Assert.Equal(expectedHeadPosition, rope.Head);
         Assert.Equal(expectedTailPosition, rope.Tail);
+        Assert.Equal(expectedVisited, rope.Visited);
+    }
+
+    [Theory]
+    [InlineData('U', 1, 'U', 1, 0, 2, 0, 1)]
+    public void MoveLongRopeTests(
+        char direction1, int distance1,
+        char direction2, int distance2,
+        int knot0X, int knot0Y,
+        int knot1X, int knot1Y)
+    {
+        var rope = new LongRope();
     }
 }
--- day9/Day9/Program.cs
+++ day9/Day9/Program.cs
@@ -1,53 +1,136 @@
-﻿using Microsoft.VisualBasic;
-
-var moves = File.ReadAllLines("data/moves.example");
+﻿var moves = File.ReadAllLines("data/moves.dat");
 var moveInputs = moves.Select(move => new MoveInput(move[0], int.TryParse(move.Substring(2), out var d) ? d : 0)).ToList();
 
-moveInputs.ForEach(m => Console.WriteLine($"{m.direction}\t{m.distance}"));
+var rope = new Rope(2);
+moveInputs.ForEach(m => rope.Move(m));
+Console.WriteLine($"Tail visited {rope.Visited} spaces.");
 
-var position = new Position(0, 0);
+var longRope = new LongRope();
+moveInputs.ForEach(m => longRope.Move(m));
+Console.WriteLine($"Tail visited {longRope.Visited} spaces.");
 
-public record MoveInput(char direction, int distance);
+public record MoveInput(char Direction, int Distance);
 
 public record Position(int X, int Y);
 
-public sealed class Rope
+// Rope from part 2
+public sealed class LongRope
 {
-    public Rope()
+    private readonly HashSet<Position> _visited;
+
+    public LongRope()
     {
-        Head = new Position(0, 0);
-        Tail = new Position(0, 0);
+        Knots = new Position[10];
+        _visited = new HashSet<Position>
+        {
+            new(0, 0)
+        };
     }
-    public Position Head { get; set; }
-    public Position Tail { get; set; }
+
+    public IList<Position> Knots { get; set; }
+
+    public int Visited => _visited.Count;
 
     public void Move(MoveInput input)
     {
-        for (var i = 0; i < input.distance; i++)
+        for (var i = 0; i < input.Distance; i++)
         {
-            if (input.direction == 'U')
+            if (input.Direction == 'U')
             {
-                Head = new Position(Head.X, Head.Y + 1);
-                if (Head.Y - 1 > Tail.Y)
-                    Tail = new Position(Head.X, Head.Y - 1);
+                Knots[0] = new Position(Knots[0].X, Knots[0].Y + 1);
+                if (Knots[0].Y - 1 > Knots[1].Y)
+                    Knots[1] = new Position(Knots[0].X, Knots[0].Y - 1);
+                _visited.Add(Knots[9]);
             }
-            else if (input.direction == 'D')
+            else if (input.Direction == 'D')
             {
-                Head = new Position(Head.X, Head.Y - 1);
-                if (Head.Y + 1 < Tail.Y)
-                    Tail = new Position(Head.X, Head.Y + 1);
+                Knots[0] = new Position(Knots[0].X, Knots[0].Y - 1);
+                if (Knots[0].Y + 1 < Knots[1].Y)
+                    Knots[1] = new Position(Knots[0].X, Knots[0].Y + 1);
+                _visited.Add(Knots[9]);
             }
-            else if (input.direction == 'R')
+            else if (input.Direction == 'R')
             {
-                Head = new Position(Head.X + 1, Head.Y);
-                if (Head.X - 1 > Tail.X)
-                    Tail = new Position(Head.X - 1, Head.Y);
+                Knots[0] = new Position(Knots[0].X + 1, Knots[0].Y);
+                if (Knots[0].X - 1 > Knots[1].X)
+                    Knots[1] = new Position(Knots[0].X - 1, Knots[0].Y);
+                _visited.Add(Knots[9]);
             }
-            else if (input.direction == 'L')
+            else if (input.Direction == 'L')
             {
-                Head = new Position(Head.X - 1, Head.Y);
-                if (Head.X + 1 < Tail.X)
-                    Tail = new Position(Head.X + 1, Head.Y);
+                Knots[0] = new Position(Knots[0].X - 1, Knots[0].Y);
+                if (Knots[0].X + 1 < Knots[1].X)
+                    Knots[1] = new Position(Knots[0].X + 1, Knots[0].Y);
+                _visited.Add(Knots[9]);
+            }
+        }
+    }
+
+    public void MoveKnots()
+    {
+        for (var i = 2; i < 10; i++)
+        {
+            if (Knots[i].X - Knots[i - 1].X > 1)
+            {
+                // Knots[i] = new Position();
+            }
+        }
+    }
+}
+
+// Rope from part 1
+public sealed class Rope
+{
+    private readonly HashSet<Position> _visited;
+
+    public Rope(int knots)
+    {
+        Knots = new Position[knots];
+        Knots[0] = new Position(0, 0);
+        Knots[1] = new Position(0, 0);
+        _visited = new HashSet<Position>
+        {
+            new(0, 0)
+        };
+    }
+
+    public Position Head => Knots[0];
+    public Position Tail => Knots[1];
+
+    public IList<Position> Knots { get; set; }
+    public int Visited => _visited.Count;
+
+    public void Move(MoveInput input)
+    {
+        for (var i = 0; i < input.Distance; i++)
+        {
+            if (input.Direction == 'U')
+            {
+                Knots[0] = new Position(Knots[0].X, Knots[0].Y + 1);
+                if (Knots[0].Y - 1 > Knots[1].Y)
+                    Knots[1] = new Position(Knots[0].X, Knots[0].Y - 1);
+                _visited.Add(Knots[1]);
+            }
+            else if (input.Direction == 'D')
+            {
+                Knots[0] = new Position(Knots[0].X, Knots[0].Y - 1);
+                if (Knots[0].Y + 1 < Knots[1].Y)
+                    Knots[1] = new Position(Knots[0].X, Knots[0].Y + 1);
+                _visited.Add(Knots[1]);
+            }
+            else if (input.Direction == 'R')
+            {
+                Knots[0] = new Position(Knots[0].X + 1, Knots[0].Y);
+                if (Knots[0].X - 1 > Knots[1].X)
+                    Knots[1] = new Position(Knots[0].X - 1, Knots[0].Y);
+                _visited.Add(Knots[1]);
+            }
+            else if (input.Direction == 'L')
+            {
+                Knots[0] = new Position(Knots[0].X - 1, Knots[0].Y);
+                if (Knots[0].X + 1 < Knots[1].X)
+                    Knots[1] = new Position(Knots[0].X + 1, Knots[0].Y);
+                _visited.Add(Knots[1]);
             }
         }
     }
