f395fa00fe9868ae57cfb2bc657ccd59ad9763e5
kevin <KCottrill088@GMail.com>
2022-12-25 09:25:14 -0600

    day11 WIP

--- day11/Day11/Program.cs
+++ day11/Day11/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System.Text.RegularExpressions;
 
 var input = File.ReadAllLines("/home/kevin/repos/advent2022/day11/Day11/data/monkeys.example");
+var monkeys = new List<Monkey>();
 input.ToList().ForEach(line =>
 {
     var monkeyLine = Regex.Match(line, @"^Monkey\s+(\d+):"); 
     if (monkeyLine.Success)
     {
+        var monkey = new Monkey(int.Parse(monkeyLine.Groups[1].Value));
+        monkeys.Add(monkey);
         Console.WriteLine(line);
-        Console.WriteLine(monkeyLine.Groups[1]);
+        Console.WriteLine(monkeys.Last().Id);
     }
+
+    var startingItems = Regex.Match(line, @"^\s+(.+)$");
+
 });
+
+internal sealed class Monkey
+{
+    public Monkey(int id)
+    {
+        Id = id;
+    }
+    public int Id { get; set; }
+}
